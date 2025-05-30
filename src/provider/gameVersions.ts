4d223890e3dc06784f19199ef127cdabfde26b51
TheKodeToad <TheKodeToad@proton.me>
2025-05-30 18:42:33 +0100

    Experimental versions

--- src/provider/gameVersions.ts
+++ src/provider/gameVersions.ts
@@ -1,28 +1,58 @@
-import { HTTPCacheMode } from "#types/httpCache.ts";
+import type { DiskHTTPCache } from "#runner/diskHTTPCache.ts";
+import { HTTPCacheMode, type HTTPCache } from "#types/httpCache.ts";
 import { PistonVersion } from "#types/pistonMeta/pistonVersion.ts";
-import { PistonVersionManifest } from "#types/pistonMeta/pistonVersionManifest.ts";
+import { PistonVersionManifest, PistonVersionRef } from "#types/pistonMeta/pistonVersionManifest.ts";
 import { defineProvider } from "#types/provider.ts";
 
 export default defineProvider({
 	id: "game-versions",
 
-	async provide(http) {
-		const manifest = PistonVersionManifest.parse(
-			await http.fetchJSONContent(
-				"piston-meta/version_manifest_v2.json",
-				"https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
-			)
-		);
-
-		return await Promise.all(manifest.versions.map(async (version): Promise<PistonVersion> => {
-			const json = await http.fetchJSONContent(
-				"piston-meta/" + version.id + ".json",
-				version.url,
-				{ mode: HTTPCacheMode.CompareLocalDigest, algorithm: "sha-1", expected: version.sha1 }
-			);
-
-			return PistonVersion.parse(json);
-		}));
+	async provide(http): Promise<PistonVersion[]> {
+		return Promise.all([pistonMetaVersions(http), fabricMavenExperimentalVersions(http)])
+			.then(versions => versions.flat());
 	}
 });
-``;
+
+async function pistonMetaVersions(http: DiskHTTPCache): Promise<PistonVersion[]> {
+	const base = "piston-meta";
+
+	const manifest = PistonVersionManifest.parse(
+		await http.fetchJSONContent(
+			base + "/versions.json",
+			"https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
+		)
+	);
+
+	return await getVersions(http, base, manifest.versions);
+}
+
+async function fabricMavenExperimentalVersions(http: DiskHTTPCache): Promise<PistonVersion[]> {
+	const base = "fabric-maven";
+
+	const experimentalManifest = PistonVersionManifest.parse(
+		await http.fetchJSONContent(
+			base + "/experimental-versions.json",
+			"https://maven.fabricmc.net/net/minecraft/experimental_versions.json"
+		)
+	);
+
+	const versions = experimentalManifest.versions
+		.filter(version => version.type === "pending");
+
+	const full = await getVersions(http, base, versions);
+
+	return full.map(version => ({ ...version, type: "experiment" }));
+}
+
+async function getVersions(http: HTTPCache, base: string, versions: PistonVersionRef[]): Promise<PistonVersion[]> {
+	return await Promise.all(versions.map(async (version): Promise<PistonVersion> => {
+		const json = await http.fetchJSONContent(
+			base + "/" + version.id + ".json",
+			version.url,
+			{ mode: HTTPCacheMode.CompareLocalDigest, algorithm: "sha-1", expected: version.sha1 }
+		);
+
+		return PistonVersion.parse(json);
+	}));
+}
+
--- src/types/pistonMeta/pistonVersionManifest.ts
+++ src/types/pistonMeta/pistonVersionManifest.ts
@@ -1,18 +1,16 @@
 import { z } from "zod/v4";
 
-export const PistonVersionManifest = z.object({
-	latest: z.object({
-		release: z.string(),
-		snapshot: z.string(),
-	}),
-	versions: z.array(z.object({
-		id: z.string(),
-		type: z.enum(["release", "snapshot", "old_beta", "old_alpha"]),
-		url: z.url(),
-		time: z.coerce.date(),
-		releaseTime: z.coerce.date(),
-		sha1: z.string().transform(input => Buffer.from(input, "hex")),
-	})),
+export const PistonVersionRef = z.object({
+	id: z.string(),
+	type: z.string(),
+	url: z.url(),
+	time: z.coerce.date(),
+	releaseTime: z.coerce.date(),
+	sha1: z.string().transform(input => Buffer.from(input, "hex")),
 });
+
+export interface PistonVersionRef extends z.output<typeof PistonVersionRef> { }
+
+export const PistonVersionManifest = z.object({ versions: z.array(PistonVersionRef) });
 
 export interface PistonVersionManifest extends z.output<typeof PistonVersionManifest> { }
