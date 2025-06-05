a41ead580deb94a80e9549a6e8e807e2207522f1
TheKodeToad <TheKodeToad@proton.me>
2025-06-05 20:44:37 +0100

    Sweet spot?

--- src/provider/neoForgeLoaderVersions.ts
+++ src/provider/neoForgeLoaderVersions.ts
@@ -40,7 +40,7 @@ async function provide(http: HTTPClient, group: string, artifact: string): Promi
 
 	const basedVersions = versions.filter(version => !CRINGE_VERSIONS.includes(version));
 
-	const limit = concurrencyLimit(8);
+	const limit = concurrencyLimit(6);
 
 	return await Promise.all(basedVersions.map(async version => {
 		const installerArtifact = MavenArtifactRef.parse(group + ":" + artifact + ":" + version + ":installer");
